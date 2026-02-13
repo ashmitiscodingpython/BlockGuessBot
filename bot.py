699d12ef7a17aefc75e64c592f3047c73e8a7e85
ashmitiscodingpython <ashmit.jeevan@gmail.com>
2026-02-13 22:19:17 +0530

    Finally got it working

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import os
+import dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+dotenv.load_dotenv()
 app = App(token='xoxb-2210535565-10464203332802-iIVPjrhLk2jZG4Hfpw7FYf3T')
 
 @app.message("ping")
@@ -10,8 +12,18 @@ def ping(ack, say):
     say("pong :table_tennis_paddle_and_ball:")
 
 @app.event("message")
-def handle_message_events(body, logger):
+def handle_message_events(body: dict, client, logger):
     logger.info(body)
+    info = body["event"]
+    if info.get("thread_ts") and info.get("parent_user_id") == "U0ADN5Z9SPL":
+        post_message(client, info["channel"], "Thready :thread:", info["thread_ts"])
+
+def post_message(client, channel, text, thread_ts=None):
+    client.chat_postMessage(
+        channel=channel,
+        text=text,
+        thread_ts=thread_ts
+    )
 
 if __name__ == "__main__":
-    SocketModeHandler(app, 'xapp-1-A0ADHBVJBUY-10467810726292-a71b25721cfd4cc7e687910ee24dec4ab117905a591ff28ebd94dc981ee465f7').start()
+    SocketModeHandler(app, os.getenv("API_KEY")).start()
